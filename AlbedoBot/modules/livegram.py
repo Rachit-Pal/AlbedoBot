3fb10b3f73486afd2519cf981ddc018cc69b6384
Rachit Pal <rachit2380@gmail.com>
2022-09-02 15:44:06 +0530

    Update livegram.py

--- AlbedoBot/modules/livegram.py
+++ AlbedoBot/modules/livegram.py
@@ -7,5 +7,5 @@ from AlbedoBot import pgram as bot
 
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
-    if not message.from_user.id == 5362971543:
+    if not message.from_user.id == 931530251:
         fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
